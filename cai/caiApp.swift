1abe8eae54795e5eed333803229056bf5a1abc2a
Daniel <dguarnizo2@gmail.com>
2024-04-11 15:17:04 +0200

    Initial Commit

---
+++ cai/caiApp.swift
@@ -0,0 +1,17 @@
+//
+//  caiApp.swift
+//  cai
+//
+//  Created by Daniel Guarnizo on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct caiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
